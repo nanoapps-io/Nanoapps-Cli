f95ed8372f1046cd8dd4296137dfb4b921cf537c
dineshswamy <dineshswamy92@gmail.com>
2017-09-09 05:43:10 +0530

    command uploads and updates the content

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import axios from "axios"
 import program from "commander"
 import shell from "shelljs"
 import FormData from "form-data"
-import $ from "jquery";
 
 program
     .command('login')
@@ -23,7 +22,7 @@ program
     .command('create')
     .description('Create nanoapps')
     .action(() => {
-        verifyProjectToken()
+        uploadBundleFile()
     });
 
 program.parse(process.argv);
@@ -73,7 +72,7 @@ function persistToken(response) {
     jsonfile.writeFileSync(dir + configs.CREDS_FILE, response)
 }
 
-function verifyProjectToken() {
+function getProjectToken() {
     // let file = homedir() + configs.NANOAPPS_FOLDER + configs.CREDS_FILE
     let file = homedir() + configs.NANOAPPS_FOLDER + configs.CREDS_FILE
     let credsFile = jsonfile.readFileSync(file)
@@ -103,32 +102,44 @@ function verifyProjectToken() {
     }
 }
 
-function uploadBundleFile(project) {
+function uploadBundleFile() {
     // let bundle_gen_cmd = 'node node_modules/react-native/local-cli/cli.js bundle --entry-file="index.android.js" --bundle-output="./main.jsbundle" --dev=false --platform="android"' 
     // if (shell.exec(bundle_gen_cmd).code !== 0) {
     //     shell.echo('Error: bundle file generation failed');
     //     shell.exit(1);
     //     return 
     // }
+    let file = homedir() + configs.NANOAPPS_FOLDER + configs.CREDS_FILE
+    let credsFile = jsonfile.readFileSync(file)
+    let authToken = credsFile["token"]
     let formData = new FormData();
-    let bundleFile = fs.createReadStream("main.jsbundle")
-    let nanoappsJson = fs.createReadStream("nanoapp.json")
+    let nanoappsJson = jsonfile.readFileSync("./nanoapp.json")
     let data = new FormData();
-    data.append('nanoapp', fs.createReadStream('./main.jsbundle'), 'main.js');
-    data.append('nanoapp', fs.createReadStream('./nanoapp.json'), 'nanoapp.json');
-    
+    data.append('project_token',nanoappsJson['project_token'])
+    data.append('name',nanoappsJson['name'])
+    data.append('package_name',nanoappsJson['package_name'])
+    data.append('version_code',nanoappsJson['version_code'])
+    data.append('version_name',nanoappsJson['version_name'])
+    data.append('description',nanoappsJson['description'])
+    data.append('bundle_file', fs.createReadStream('./main.jsbundle'), 'main.js');
+    data.append('app_icon', fs.createReadStream(nanoappsJson['app_icon']), nanoappsJson['app_icon']);
     let options = {
         method: 'POST',
         url: 'http://localhost:8090/api/nanoapp/upload',
         headers: {
-            'Content-Type': `multipart/form-data; boundary=${data._boundary}`
+            'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+            'X-Nanoapp-Token': 'Bearer ' + authToken
         },
         data
     };
-    
     return axios(options)
         .then(response => {
-            console.log(response);
+            console.log(response.data);
+        }).catch(function (error) {
+            console.log(error);
         });
+}
 
+function validateNanoappsJsonFile(json) {
+    console.log("Validation nanoapps json file")
 }
